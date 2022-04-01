9ddf603a6654cd931a796b455461f76e5f166b73
sebokdas1 <sebok.das.ff@gmail.com>
2022-04-01 09:17:43 +0600

    custom link added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
         <div>
             <h2>Welcome To TSHIRT-MANIA!!</h2>
+            <div>
+                <CustomLink to='/home'>Home</CustomLink>
+                <CustomLink to='/orderreview'>Order Review</CustomLink>
+            </div>
         </div>
     );
 };
