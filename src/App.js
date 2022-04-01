8da72f949a3b1510e894460daaa3d49a92ead7b1
sebokdas1 <sebok.das.ff@gmail.com>
2022-04-01 09:00:04 +0600

    components created"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import OrderReview from './components/OrderReview/OrderReview';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/orderreview' element={<OrderReview />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Welcome To TSHIRT-MANIA!!</h2>
+        </div>
+    );
+};
+
+export default Header;
