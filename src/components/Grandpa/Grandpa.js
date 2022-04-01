2a0ff3422d6a03d65480d85d16c096ef6abd03a2
sebokdas1 <sebok.das.ff@gmail.com>
2022-04-01 17:14:48 +0600

    use react context

--- src/components/Grandpa/Grandpa.js
+++ src/components/Grandpa/Grandpa.js
@@ -1,26 +1,31 @@
-import React, { useState } from 'react';
+import React, { createContext, useState } from 'react';
 import Aunty from '../Aunty/Aunty';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import './Grandpa.css'
 
+export const GiftContext = createContext('ring')
 const Grandpa = () => {
 
     const [house, setHouse] = useState(1);
+
+
     const handleBuyAHouse = () => {
         const newHouse = house + 1;
         setHouse(newHouse)
     }
     return (
-        <div className='grandpa'>
-            <h3>Grandpa</h3>
-            <p>House: {house}  <button onClick={handleBuyAHouse}>buy a house</button></p>
-            <section style={{ display: 'flex' }}>
-                <Father house={house}></Father>
-                <Uncle house={house}></Uncle>
-                <Aunty house={house}></Aunty>
-            </section>
-        </div>
+        <GiftContext.Provider value='Diamond Ring'>
+            <div className='grandpa'>
+                <h3>Grandpa</h3>
+                <p>House: {house}  <button onClick={handleBuyAHouse}>buy a house</button></p>
+                <section style={{ display: 'flex' }}>
+                    <Father house={house}></Father>
+                    <Uncle house={house}></Uncle>
+                    <Aunty house={house}></Aunty>
+                </section>
+            </div>
+        </GiftContext.Provider>
     );
 };
 
--- src/components/Special/Special.js
+++ src/components/Special/Special.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GiftContext } from '../Grandpa/Grandpa';
 
 const Special = () => {
+    const gift = useContext(GiftContext)
     return (
         <div>
             <h5>Special</h5>
+            <p>Gift: {gift}</p>
         </div>
     );
 };
